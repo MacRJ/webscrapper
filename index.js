eb6b5479aa6d85ac4d052f2bbc92767621fa476f
Richard Jones <rmacwj@gmail.com>
2021-03-06 22:33:59 -0700

    removed browser being passed down

--- index.js
+++ index.js
@@ -1,21 +1,34 @@
 const writeToFile = require('./functions/writeToFile');
-const targetWebsite = require('./config').targetWebsite;
+const targetWebsiteTemp = require('./config').targetWebsiteTemp;
 const targetElement = require('./config').targetElement;
-const {openPage, closePage} = require('./functions/pageControl');
+const {openPage, closePage, closeBrowser} = require('./functions/pageControl');
+const {mainDataPage} = require('./mainDataPage');
 
 (async () => {
   const allWorkouts = {};
-  let {page, browser} = await openPage(targetWebsite);
+  let {page, browser} = await openPage(targetWebsiteTemp);
   
   const mainElements = await page.$$(targetElement);
+  let singleElement = mainElements[0];
 
-  for (let singleElement of mainElements) {
+//   for (let singleElement of mainElements) {
       let inputField = await singleElement.$eval('input[type=checkbox]', el => el.value);
       let category = await singleElement.$eval('.ExCategory-formLabelLabel', el => el.innerText);
-  }
+
+      closePage(page);
+
+    //   console.log('inputField', inputField);
+      try {
+          //Should return an array of objects {name, workoutType, imageURL}
+          await mainDataPage(inputField)
+      }
+      catch(error) {
+          console.log(error)
+      }
+//   }
 
 
-  await closePage(browser)
+  await closeBrowser();
   console.log('off')
 
 //todo: This is where the object gets saved as a json object -- first build the object
---
+++ mainDataPage.js
@@ -0,0 +1,30 @@
+const {targetWebsite, targetDataPageTemp} = require('./config');
+const {newPage, closePage, closeBrowser} = require('./functions/pageControl');
+
+//https://www.bodybuilding.com/exercises/finder/?muscle=chest
+exports.mainDataPage = async function(targetWebsite) {
+    let arrayOfWorkouts = [];
+    // const newPage = targetWebsite + '?muscle=' + page;
+    const newPagepage = targetDataPageTemp;
+    
+    const {page} = await newPage(newPagepage);
+
+    const allWorkouts = await page.$$('.ExResult-row');
+
+    for (const workout of allWorkouts) {
+        console.log('here3')
+        const newObj = {};
+        const name = await workout.$eval('link[itemprop=name]', el => el.innerText);
+        console.log({name})
+        //name
+            // newObj[]
+
+        //workoutType
+
+        //imageURL
+    };
+
+    await closeBrowser();
+    console.log('finished');
+
+};
