f843d38f0c31fe79242c25d8f2cdb04809231cb0
Richard Jones <rmacwj@gmail.com>
2021-03-06 22:34:18 -0700

    refactored pageControl

--- functions/pageControl.js
+++ functions/pageControl.js
@@ -1,12 +1,25 @@
 const puppeteer = require('puppeteer');
 
+let browser;
 exports.openPage = async function(targetWebsite) {
-    const browser = await puppeteer.launch();
+    console.log('mainTargetPage', targetWebsite);
+    browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto(targetWebsite);
-    return {page, browser};
+    return {page};
 }
 
-exports.closePage = async function(browser) {
+exports.newPage = async function(targetWebsite) {
+    console.log('targetWebpage', targetWebsite);
+    const page = await browser.newPage();
+    await page.goto(targetWebsite);
+    return {page}
+};
+
+exports.closePage = async function(page) {
+    return page.close()
+}
+
+exports.closeBrowser = async function() {
      return await browser.close();
 }
