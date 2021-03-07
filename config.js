72303facc314706b38edb25cc29291ad112a5ed7
Richard Jones <rmacwj@gmail.com>
2021-03-06 22:32:20 -0700

    updated config file

--- config.js
+++ config.js
@@ -2,11 +2,15 @@ const baseRoute = require('./environmentVarables').BASE_FILE_ROUTE;
 
 const fileName = 'newTesty.json';
 // const targetWebsite = 'https://www.bodybuilding.com/exercises/finder/';
-const targetWebsite = `${baseRoute}/Exercise_Page/BODYBUILDING_HOMEPAGE.html`
+
+//for testing
+const targetWebsiteTemp = `${baseRoute}/Exercise_Page/BODYBUILDING_HOMEPAGE.html`
+const targetDataPageTemp = `${baseRoute}/Exercise_Page/EXERCISE_PAGE.html`
 
 // const targetElement = '.ExCategory-formLabelLabel';
 const targetElement = '.ExCategory-formLabel'
 
 exports.fileName = fileName;
-exports.targetWebsite = targetWebsite;
-exports.targetElement = targetElement
+exports.targetWebsiteTemp = targetWebsiteTemp;
+exports.targetElement = targetElement;
+exports.targetDataPageTemp = targetDataPageTemp;
